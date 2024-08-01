58c4cd1ece414802fe44ad7982778b75e994ddea
shikso81 <nj.company.777@gmail.com>
2024-08-01 10:47:58 +0500

    Update T.E.W.py

--- T.E.W.py
+++ T.E.W.py
@@ -267,16 +267,7 @@ def disable_resize_and_maximize():
     WS_MAXIMIZEBOX = 0x00010000
 
 
-    hwnd = ctypes.windll.kernel32.GetConsoleWindow()
-
-
-    style = ctypes.windll.user32.GetWindowLongW(hwnd, GWL_STYLE)
-
-    style = style & ~WS_SIZEBOX
-    style = style & ~WS_MAXIMIZEBOX
-
-    ctypes.windll.user32.SetWindowLongW(hwnd, GWL_STYLE, style)
-
+    
 
 
 
